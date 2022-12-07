8d3539e4c137e0a6d14047a2aef5899b4f14391b
jmkim0 <jmkim000000@gmail.com>
2022-12-07 15:01:08 +0900

    First commit

---
+++ src/main/java/com/codestates/bedeploymentgithubaction/BeDeploymentGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.bedeploymentgithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDeploymentGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeDeploymentGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/bedeploymentgithubaction/BeDeploymentGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.bedeploymentgithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDeploymentGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
