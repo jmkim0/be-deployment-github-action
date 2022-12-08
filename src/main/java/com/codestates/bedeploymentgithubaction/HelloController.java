e258b7aacc1b17608ae087ea27fe7518a1d1c9f0
jmkim0 <jmkim000000@gmail.com>
2022-12-08 13:55:32 +0900

    Update HelloController

--- src/main/java/com/codestates/bedeploymentgithubaction/HelloController.java
+++ src/main/java/com/codestates/bedeploymentgithubaction/HelloController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private int count = 1;
     @GetMapping("/")
     public String hello() {
-        return "Hello world!";
+        return "<h1>Hello world!</h1>Count: " + count++;
     }
 }
